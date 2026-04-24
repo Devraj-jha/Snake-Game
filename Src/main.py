6f4a1425ade0a17198e245ac3b280cb26fef3580
Devraj-jha <devrajjha100806@gmail.com>
2026-04-24 11:23:05 +0530

    created a white square and moved it

--- Src/main.py
+++ Src/main.py
@@ -2,14 +2,21 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((400,400))
-# clock = pygame.time.Clock()
+clock = pygame.time.Clock()
 running = True
+x = 100
+y = 100
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     screen.fill((0,0,0))
+
+    x -= 1
+    y -= 1
+    pygame.draw.rect(screen, "white", (x, y, 25, 25))
+
     pygame.display.update()
+    clock.tick(60)
 
 pygame.quit()
