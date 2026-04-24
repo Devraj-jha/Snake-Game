43438d869000d41d63d15dec75ae051a35df7d84
Devraj-jha <devrajjha100806@gmail.com>
2026-04-24 10:52:41 +0530

    window creation

---
+++ Src/main.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+# clock = pygame.time.Clock()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0,0,0))
+    pygame.display.update()
+
+pygame.quit()
